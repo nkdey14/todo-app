204fc7dd8f47369ed572c29e6186e43a318ed4fe
nkdey14 <im.pandey264@gmail.com>
2026-07-23 16:25:35 +0530

    Supplying value from Parent AppComponent to child UserComponent using @Input to display User name and avatar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from './user/dummy-users';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'demo-app';
+  users = DUMMY_USERS;
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, Input, signal } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
@@ -9,7 +9,7 @@ import { DUMMY_USERS } from './dummy-users';
 export class UserComponent {
   selectedUsers = signal(DUMMY_USERS[0]);
 
-  imagePath = computed(() => 'assets/' + this.selectedUsers().avatar);
+  // imagePath = computed(() => 'assets/' + this.selectedUsers().avatar);
 
   // Event binding for selected users
   onSelectedUsers() {
@@ -18,8 +18,11 @@ export class UserComponent {
     this.selectedUsers.set(DUMMY_USERS[randomIndex]);
   }
 
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+
   // Added getter for storing path for image/avatar for users
-  // get imagePath() {
-  //   return 'assets/' + this.selectedUsers().avatar;
-  // }
+  get imagePath() {
+    return 'assets/' + this.avatar;
+  }
 }
