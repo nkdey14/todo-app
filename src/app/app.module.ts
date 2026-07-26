b610af2116919162cb2b876e47df28432dd8552b
nkdey14 <im.pandey264@gmail.com>
2026-07-26 14:24:26 +0530

    Created TasksComponent for Adding Task for each user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { TaskComponent } from './tasks/task/task.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, TasksComponent],
+  declarations: [AppComponent, UserComponent, TasksComponent, TaskComponent],
   imports: [BrowserModule, AppRoutingModule, HeaderComponent],
   providers: [],
   bootstrap: [AppComponent],
